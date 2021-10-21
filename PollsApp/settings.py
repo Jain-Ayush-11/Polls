acef06e57e7678bb16e1b99fc8ecdfc973d4f77a
AlStien <aj601604@gmail.com>
2021-10-21 23:53:01 +0530

    configured database and admin pannel | Documentation Part 2

--- PollsApp/settings.py
+++ PollsApp/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'polls.apps.PollsConfig'
 ]
 
 MIDDLEWARE = [
@@ -108,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'IST'
+TIME_ZONE = 'GMT'
 
 USE_I18N = True
 
